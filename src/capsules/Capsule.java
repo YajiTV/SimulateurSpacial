8c55b4e1dac8439e732d26c76734c00831df874a
pmathys <mathys.paillekrobb@ynov.com>
2026-04-27 17:03:57 +0200

    feat(Capsule) : added capsule abstract class with validation, getter and string

--- src/capsules/Capsule.java
+++ src/capsules/Capsule.java
@@ -1,1 +1,57 @@
 package capsules;
+
+public abstract class Capsule {
+    private String name;
+    private boolean crewed;
+    private int maxOccupants;
+    private double price;
+    private double weight;
+
+    public Capsule(String name, boolean crewed, int maxOccupants, double price, double weight) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name can not be null or empty");
+        }
+        if (maxOccupants < 0) {
+            throw new IllegalArgumentException("Max occupants cannot be negative");
+        }
+        if (price < 0) {
+            throw new IllegalArgumentException("Price cannot be negative");
+        }
+        if (weight <= 0) {
+            throw new IllegalArgumentException("Weight must be greater than 0");
+        }
+
+        this.name = name;
+        this.crewed = crewed;
+        this.maxOccupants = maxOccupants;
+        this.price = price;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public boolean isCrewed() {
+        return crewed;
+    }
+    public int getMaxOccupants() {
+        return maxOccupants;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Capsule{"
+                + "name='" + name + "'"
+                + ", crewed=" + crewed
+                + ", maxOccupants=" + maxOccupants
+                + ", price=" + price + "M€"
+                + ", weight=" + weight + "t"
+                + '}';
+    }
+}
